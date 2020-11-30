2e1df2b74a25e3ab8480519567898a1da975bae9
OlegSuhomerov <44775507+OlegSuhomerov@users.noreply.github.com>
2020-11-30 17:18:42 +0300

    changed button animation

--- scripts/modules/animate_button.lua
+++ scripts/modules/animate_button.lua
@@ -10,19 +10,19 @@ function animate_button.animation(id, self, action_id, action)
 	local node = gui.get_node(id)
 	if action_id == game_messages.touch and gui.pick_node(node, action.x, action.y) then
 		if play_anim == false then
-			gui.animate(node, game_messages.scale, 0.8, gui.PLAYBACK_LOOP_FORWARD, 0.0)
+			gui.animate(node, game_messages.scale, 0.8, gui.PLAYBACK_LOOP_FORWARD, 0.3)
 			self.start_color = gui.get_color(node)
 			local anim_color = self.start_color * config.blackout
 			anim_color.w = 1
-			gui.animate(node, game_messages.color, anim_color, gui.PLAYBACK_LOOP_FORWARD, 0.0)
+			gui.animate(node, game_messages.color, anim_color, gui.PLAYBACK_LOOP_FORWARD, 0.3)
 			play_anim = true
 		end
 	end
 	if action.released then
-		gui.animate(node, game_messages.scale, 1, gui.PLAYBACK_LOOP_FORWARD, 0.0, 0, function()
+		gui.animate(node, game_messages.scale, 1, gui.PLAYBACK_LOOP_FORWARD, 0.3, 0, function()
 			play_anim = false
 		end)
-		gui.animate(node, game_messages.color, self.start_color, gui.PLAYBACK_LOOP_FORWARD, 0.0)
+		gui.animate(node, game_messages.color, self.start_color, gui.PLAYBACK_LOOP_FORWARD, 0.3)
 	end
 end
 	
